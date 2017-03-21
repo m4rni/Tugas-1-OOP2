fd0e15d7bb39532d4dcde4527fe58e28e1e5985c
m4rni <marnisu66@gmail.com>
2017-03-21 19:27:21 +0700

    Java

---
+++ InputOutput3.java
@@ -0,0 +1,10 @@
+import java.util.scanner;
+public class InputOutput3 {
+	public static void main (String[] args){
+	Scanner input = new Scanner (System.in);
+	String nama;
+
+	nama = input.nextLine();
+	nama = System.out.print(nama);
+	}
+}
