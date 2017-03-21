850a4c1af0e0f42b299279a857477653e0f2168a
m4rni <marnisu66@gmail.com>
2017-03-21 19:24:13 +0700

    Add files via upload

---
+++ InputOutput1.java
@@ -0,0 +1,7 @@
+public class InputOutput1{
+	public static void main (String[] args) {
+	System.out.print("Belajar java yuuuks");
+	System.out.println("Belajar java yuuks 1"); 
+	System.out.print("Belajar java yuuks 2");
+	}
+}
